2b698720d2bfec20b6e22464632e434b7b6667c7
Ryodookie <51737445+Ryodookie@users.noreply.github.com>
2021-01-26 00:03:59 +0100

    made an app following a tutorial

--- converter.py
+++ converter.py
@@ -1,1 +1,24 @@
-print("Hello there Marci")
+import imageio
+import os
+
+video = os.path.abspath("Assembly.mp4")
+
+def gifMaker(inputPath, targetFormat):
+    outputPath = os.path.splittext(inputPath)[0] + targetFormat
+    
+    print(f"converting {inputPath} /n to {outputPath}")
+
+    reader = imageio.get_reader(inputPath)
+    fps = reader.get_meta_data()["fps"]
+
+    writer = imageio.get_writer(outputPath, fps=fps)
+
+    for frames in reader:
+        writer.append_data(frames)
+        print(f"Frame{frames}")
+    print("done")
+    writer.close()
+
+gifMaker(video, ".gif")
+
+
